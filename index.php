42c62139bfbf17f9c95fded0005764435f40d312
Joseph <josefc9512@gmail.com>
2021-03-23 11:24:49 -0500

    add index.php and folder for templates pages

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php 
+	get_header();
+	// File base.
+?>
+	<!-- Include a component -->
+	<?php include get_theme_file_path('includes/component.php');?>
+
+<?php get_footer();?>
---
+++ templates/example.php
@@ -0,0 +1,8 @@
+<?php 
+	/* Template Name: Example */ 
+	get_header();
+?>
+	<!-- Include a component -->
+	<?php include get_theme_file_path('includes/component.php');?>
+
+<?php get_footer();?>
